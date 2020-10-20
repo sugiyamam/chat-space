a42b92ed68498304889c7412f4942b4c1935a563
sugiyamam <ms57926@gmail.com>
2020-10-20 15:21:13 +0900

    自動デプロイ実行用2

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :application, 'chat-space'
 set :repo_url, 'git@github.com:sugiyamam/chat-space.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 set :linked_files, %w{ config/secrets.yml }
 
 set :rbenv_type, :user
